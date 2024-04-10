040fec492c9d53eb3f88abd316913004a7a773b2
alejo190404 <alejo190404@gmail.com>
2024-04-09 23:48:37 -0500

    Versión funcional. No implementa restricciones

--- savings_algorithm.py
+++ savings_algorithm.py
@@ -19,6 +19,47 @@ def cargar_laberinto(ruta_archivo):
             laberinto.append(fila)
     return laberinto
 
+#Función para saber si un elemento está en un arreglo
+def estaEnArreglo(arreglo, elemento):
+    for i in range(len(arreglo)):
+        if (arreglo[i] == elemento):
+            return True
+    return False
+
+def estaEnMatriz(matriz, elemento):
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if (matriz[i][j] == elemento):
+                return True
+    return False
+
+#Función para anadir un punto a una ruta
+def anadirPuntoARuta(ruta, punto):
+    if (estaEnArreglo(ruta, punto[0])):
+        #El punto no es interior desde el comienzo o desde el final
+        if(ruta.index(punto[0]) == 1):
+            ruta.insert(ruta.index(punto[0]) , punto[1])
+        else:
+            ruta.insert(ruta.index(punto[0]) + 1, punto[1])
+    else:
+        if(ruta.index(punto[1]) == 1):
+            ruta.insert(ruta.index(punto[1]), punto[0])
+        else:
+            ruta.insert(ruta.index(punto[1]) + 1, punto[0])
+        
+
+def fusionar_rutas(ruta1, ruta2, first_key):
+    ruta_fusionada = []
+    for i in range(len(ruta1)):
+        if (ruta1[i] == first_key[0] or ruta1[i] == first_key[1]):
+            ruta_fusionada.append(ruta1[i])
+            break
+        ruta_fusionada.append(ruta1[i])
+    if (ruta2.index(first_key[0]) == 1 or ruta2.index(first_key[1]) == len(ruta2) - 2):
+        for i in ruta2[1:]:
+            ruta_fusionada.append(i)
+    return ruta_fusionada
+
 def clarke_wright(matriz_distancia):
     n = len(matriz_distancia)
     ahorros = np.zeros((n, n))
@@ -27,27 +68,103 @@ def clarke_wright(matriz_distancia):
     imprimirMatriz(matriz_distancia)
 
     #Calcular una lista para el ahorro
-    savings_list = OrderedDict()
+    lista_ahorro = OrderedDict()
     for i in range(1, n):
         for j in range(i + 1, n):
-            savings_list[(i + 1, j + 1)] = matriz_distancia[i][0] + matriz_distancia[0][j] - matriz_distancia[i][j]
+            lista_ahorro[(i + 1, j + 1)] = matriz_distancia[i][0] + matriz_distancia[0][j] - matriz_distancia[i][j]
 
     #Ordenar la lista de ahorro
-    savings_list = OrderedDict(sorted(savings_list.items(), key=lambda x: x[1], reverse=True))
+    lista_ahorro = OrderedDict(sorted(lista_ahorro.items(), key=lambda x: x[1], reverse=True))
 
     print("Savings list:")
-    print(savings_list)
+    print(lista_ahorro)
 
+    routes = [[]]
     
 
-# Start of the program
+    while(lista_ahorro):
+        #Extraer el primer elemnto de la lista
+        for key, value in lista_ahorro.items():
+            first_key = key
+            first_value = value
+            break
+            
+        #Añadir las restricciones que se deseen
+        #En este caso, no se seleccionan restricciones, aparte de ahorrar
+
+        #Condición A: Ninguna de los puntos ha sido incluído en una ruta
+        if (not(estaEnMatriz(routes, first_key[0])) and not(estaEnMatriz(routes, first_key[1]))):
+            routes.append([1, first_key[0], first_key[1], 1])
+
+        #Condición B:
+        #Existe en una ruta existente
+        if (estaEnMatriz(routes, first_key[0]) and not(estaEnMatriz(routes, first_key[1]))):
+            #Encontrar la ruta en la que el punto se encuentra
+            for route in routes:
+                if (estaEnArreglo(route, first_key[0])):
+                    #El punto no es interior a la ruta
+                    if (route.index(first_key[0]) == 1 or route.index(first_key[0]) == len(route) - 2):
+                        #Añadir el punto a la ruta actual
+                        route = anadirPuntoARuta(route, first_key)
+                        break
+        
+
+        if (not(estaEnMatriz(routes, first_key[0])) and estaEnMatriz(routes, first_key[1])):
+            for route in routes:
+                if (estaEnArreglo(route, first_key[1])):
+                    if (route.index(first_key[1]) == 1 or route.index(first_key[1]) == len(route) - 2):
+                        route = anadirPuntoARuta(route, first_key)
+                        break
+
+        #Doble AND para condición c
+        if ((estaEnMatriz(routes, first_key[0])) and (estaEnMatriz(routes, first_key[1]))):
+            #Ninguno de los puntos es interior a la ruta en la que están
+            for route1 in routes:
+                if(estaEnArreglo(route1, first_key[0]) and not(estaEnArreglo(route1, first_key[1]))):
+                    if (route1.index(first_key[0]) <= 1 and route1.index(first_key[0]) >= len(route1) - 2):
+                        for route2 in routes:
+                            if(not(estaEnArreglo(route2, first_key[0])) and estaEnArreglo(route2, first_key[1])):
+                                if (route2.index(first_key[1]) <= 1 and route2.index(first_key[1]) >= len(route2) - 2):
+                                    pass
+                                    #Fusionar ambas rutas, siempre y cuando cumplan las restricciones
+                                    routeFusionada = fusionar_rutas(route1, route2, first_key)
+                                    routes.remove(route1)
+                                    routes.remove(route2)
+                                    routes.append(routeFusionada)
+                                    break
+                            break
+                    break
+                if(not(estaEnArreglo(route1, first_key[0])) and estaEnArreglo(route1, first_key[1])):
+                    if (route1.index(first_key[1]) > 1 and route1.index(first_key[1]) > 1):
+                        for route2 in routes:
+                            if(estaEnArreglo(route2, first_key[0]) and not(estaEnArreglo(route2, first_key[1]))):
+                                if (route2.index(first_key[0]) > 1 and route2.index(first_key[1]) < len(route2) - 2):
+                                    pass
+                                    #Fusionar ambas rutas, siempre y cuando cumplan las restricciones
+                                    routeFusionada = fusionar_rutas(route1, route2, first_key)
+                                    routes.remove(route1)
+                                    routes.remove(route2)
+                                    routes.append(routeFusionada)
+                                    break
+                            break
+                    break
+
+            
+
+        #Eliminar la lista de rutas
+        lista_ahorro.pop(first_key)
+    
+    print("Camino encontrado:")
+    return routes.pop(1)
+    
+#Comienzo del programa
 if len(sys.argv) != 2:
-    print("Usage: ahorros_algorithm.py <file with matrix>")
+    print("Usage: savings_algorithm.py <file with matrix>")
     sys.exit(1)
 
 file_path = sys.argv[1]
 
-# Load the matrix from the file
+#Cargar matriz desde el archivo
 matriz_distancia = cargar_laberinto(file_path)
 
-clarke_wright(matriz_distancia)
+print(clarke_wright(matriz_distancia))
